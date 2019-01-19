8a95b140fe64f50393f1f21a879f8415a2442ff1
Alexander <weidi5858258@sina.com>
2019-01-19 22:26:11 +0800

    update .gitignore

--- main.cpp
+++ main.cpp
@@ -26,6 +26,13 @@ int main(int argc, char *argv[]) {
     sp<ProcessState> proc(ProcessState::self());
     sp<IServiceManager> sm = defaultServiceManager();
 
+    /***
+     在android启动的时候,由系统启动时的init进程启动其他服务.
+     MyDaemon作为一个服务从android开机启动后一直常驻后台.
+     通过binder通信方式跟其他进程进行通信.
+     如java代码通过jni方式执行native层代码,然后native层代码
+     通过binder方式跟MyDaemon进行通信.
+     */
     //addService inside in BinderService
     //先执行BnDaemon的构造方法
     //再执行MyDaemon的构造方法
