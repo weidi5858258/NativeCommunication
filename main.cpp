4b2a3eee4e68467a232ae2a68625989e2cc22b92
Alexander <weidi5858258@sina.com>
2019-01-20 10:54:28 +0800

    "20190120105413"

--- main.cpp
+++ main.cpp
@@ -27,15 +27,16 @@ int main(int argc, char *argv[]) {
     sp<IServiceManager> sm = defaultServiceManager();
 
     /***
-     在android启动的时候,由系统启动时的init进程启动其他服务.
+     在android启动的时候,由系统启动时的init进程启动其他服务,
+     这些服务需要在init.rc中进行配置过.
      MyDaemon作为一个服务从android开机启动后一直常驻后台.
      通过binder通信方式跟其他进程进行通信.
      如java代码通过jni方式执行native层代码,然后native层代码
      通过binder方式跟MyDaemon进行通信.
      */
-    //addService inside in BinderService
-    //先执行BnDaemon的构造方法
-    //再执行MyDaemon的构造方法
+    // addService inside in BinderService
+    // 先执行BnDaemon的构造方法
+    // 再执行MyDaemon的构造方法
     MyDaemon::instantiate();
 
     ProcessState::self()->startThreadPool();
